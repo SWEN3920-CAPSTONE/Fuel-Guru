c143ac2880c87fd2ba0bbe4e82ba5826cfebf5ad
richtwin567 <anakai.richards@gmail.com>
2022-04-15 15:52:15 -0500

    remove dbug print

--- server.py
+++ server.py
@@ -2,7 +2,6 @@
 from config import app, db
 
 
-if __name__ == '__main__':                              
-    print(app.url_map)
+if __name__ == '__main__':
     app.run(debug=app.config.get('DEBUG_ON'), host=app.config.get(
         'HOST'), port=app.config.get('PORT'))
