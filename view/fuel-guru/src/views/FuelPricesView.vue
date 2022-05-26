c5688eb3519cf2b4a6ca553f7941a4a4eb8866cf
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2022-05-26 17:11:07 -0500

    updated css and fuelprices.vue

--- view/fuel-guru/src/views/FuelPricesView.vue
+++ view/fuel-guru/src/views/FuelPricesView.vue
@@ -1,30 +1,97 @@
 <template>
 <main id="fuelprices-area">
   <div id="cheapest-prices">
-    <div id="cheapest-87">87
+    <div id="cheapest-87">
+      <div id="cheapest-87-h">
+        <h2>Lowest <br> E-10 87 Fuel</h2>
+      </div>      
+      <div>
+        <p>GAS STATION NAME <br> GAS STATION LOCATION <br> GAS PRICE</p>
+        <button id="location-87">View Location</button>
+      </div>
     </div>
-    <div id="cheapest-90">90
+    <div id="cheapest-90">
+      <div id="cheapest-90-h">
+        <h2>Lowest <br> E-10 90 Fuel</h2>
+      </div>      
+      <div>
+        <p>GAS STATION NAME <br> GAS STATION LOCATION <br> GAS PRICE</p>
+        <button id="location-90">View Location</button>
+      </div>
     </div>
-    <div id="cheapest-diesel">diesel
+    <div id="cheapest-d">
+      <div id="cheapest-d-h">
+        <h2>Lowest <br>Diesel Fuel</h2>
+      </div>      
+      <div>
+        <p>GAS STATION NAME <br> GAS STATION LOCATION <br> GAS PRICE</p>        
+        <button id="location-d">View Location</button>
+      </div>
     </div>
-    <div id="cheapest-ulsd">usld
+    <div id="cheapest-sd">
+      <div id="cheapest-sd-h">
+        <h2>Lowest <br> ULSD Fuel</h2>
+      </div>      
+      <div>
+        <p>GAS STATION NAME <br> GAS STATION LOCATION <br> GAS PRICE</p>
+        <button id="location-sd">View Location</button>
+      </div>
     </div>
   </div>
   <div id="search-area">     
     <input type="text"  id="search-fp" placeholder="Search..">
     <button id="search-btn">Search</button>
-    <div id="filter-area">    
-    </div>  
-  </div> 
+  </div>
+  <div id="filter-area">    
+    <label for="parish">PARISH: </label>
+    <select id="parish">
+      <option>Please select one</option>
+      <option>Clarendon</option>
+      <option>Hanover</option>
+      <option>Kingston & St. Andrew</option>
+      <option>Manchester</option>
+      <option>Portland</option>
+      <option>St. Ann</option>
+      <option>St. Catherine</option>
+      <option>St. Elizabeth</option>
+      <option>St. James</option>
+      <option>St. Mary</option>
+      <option>St. Thomas</option>
+      <option>Trelawny</option>
+      <option>Westmoreland</option>
+    </select>
+    <label for="company">COMPANY: </label>
+    <select id="company">
+      <option>Please select one</option>
+      <option>Blaze</option>
+      <option>Cool Oasis</option>
+      <option>Fesco</option>
+      <option>Independent</option>
+      <option>Rubis</option>
+      <option>Texaco</option>
+      <option>Total</option>
+      <option>Unipet</option>
+    </select>
+    <label for="sortby">SORT BY: </label>
+    <select id="sortby"> 
+      <option>Please select one</option>
+      <option>Price</option>
+      <option>Name</option>
+    </select>
+  </div>  
   <div id="results-area">     
+    <!--v-for to display results-->
+    <p>S</p>
   </div>     
 </main>  
 </template>
 
+<!--
 <script>
 ///"/gasstations/search"
 
 //access control issue '(Access-Control-Allow-Origin)
+/*
 export default {
   methods: {
     getGasStations() {
@@ -46,9 +113,9 @@ export default {
 created() {
   this.getGasStations()
 }
-}
+}*/
 </script>
-
+-->
 <style>
 main {
   padding-top: 50px; 
@@ -58,6 +125,7 @@ main {
 
 #fuelprices-area {
   display: block;
+  padding-bottom: 20px;
 }
 
 #cheapest-prices {
@@ -67,15 +135,31 @@ main {
   column-gap: 100px;
 }
 
-#cheapest-prices div {
+#cheapest-87, #cheapest-90, #cheapest-d, #cheapest-sd {
   border: 2px solid #AA1414;
-  border-radius: 5px;
-  padding: 5px;
+  border-radius: 25px;
+  padding-bottom: 15px;
+}
+
+#cheapest-87-h, #cheapest-90-h, #cheapest-d-h, #cheapest-sd-h {
+  padding-left: 20px;
+  padding-right: 20px;
+}
+
+#cheapest-87 h2, #cheapest-90 h2, #cheapest-d h2, #cheapest-sd h2 {
+  border-bottom: 2px solid #AA1414;
 }
 
 #search-area {
-  padding-top: 50px;
+  padding-top: 70px;
   text-align: center;
+  padding-bottom: 20px;
+}
+
+#filter-area {
+  padding-top: 30px;
+  text-align: center;
+  padding-bottom: 20px;
 }
   
 #search-fp {
@@ -90,5 +174,22 @@ main {
   padding-right: 3%;
   color: white;
   background-color: #AA1414;
+  border-radius: 5px;
+}
+
+#location-87, #location-90, #location-d, #location-sd {
+  border: 2px solid #AA1414;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  padding-left: 3%;
+  padding-right: 3%;
+  color: white;
+  background-color: #AA1414;
+  border-radius: 25px;
+}
+
+label {
+  padding-right: 5px;
+  padding-left: 30px;
 }
 </style>
